5d31f7c6a768c9df4d515aa33d7ec355260dacde
Anthony <qdouble@gmail.com>
2016-09-18 23:53:42 -0700

    Universal updates (fixes md-input and preboot bugs and enhances performance) (#25)
    * don't allow dependencies to auto update
    
    * update universal packages
    
    * update preboot setting
    
    * put back md-input and set styles

--- src/server.ts
+++ src/server.ts
@@ -43,7 +43,7 @@ function ngApp(req, res) {
     req,
     res,
     ngModule: AppModule,
-    preboot: {},
+    preboot: true,
     baseUrl: '/',
     requestUrl: req.originalUrl,
     originUrl: req.hostname
