4cae7d35f4fca73986d1241364b380aea1b0d9b5
SaschaJankowiak <sascha.jankowiak@gmail.com>
2016-09-21 20:29:50 +0200

    added ie-shims (#39)

--- src/polyfills.browser.aot.ts
+++ src/polyfills.browser.aot.ts
@@ -1,3 +1,4 @@
 import 'core-js/es7/reflect';
+import 'core-js/client/shim';
 import 'zone.js/dist/zone';
 import 'ts-helpers';
--- src/polyfills.browser.ts
+++ src/polyfills.browser.ts
@@ -1,5 +1,6 @@
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
+import 'core-js/client/shim';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
 import 'ts-helpers';
