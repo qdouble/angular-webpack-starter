fed7637d9d926e0bdfd3a5951d96328d819f09e9
Anthony <qdouble@gmail.com>
2016-09-22 21:40:13 -0700

    cleanup

--- prodserver.js
+++ prodserver.js
@@ -2,12 +2,12 @@ const compression = require('compression')
 const express = require('express'),
 path = require('path');
 
-const ports = require('./constants')
+const PROD_PORT = require('./constants').PROD_PORT;
 
 const app = express();
 const ROOT = path.join(path.resolve(__dirname, '..'));
 
-app.use(compression())
+app.use(compression());
 app.use(express.static('dist/client'));
 
 const renderIndex = (req, res) => {
@@ -16,7 +16,7 @@ const renderIndex = (req, res) => {
 
 app.get('/*', renderIndex);
 
-var PORT = process.env.PORT || ports.PROD_PORT;
+var PORT = process.env.PORT || PROD_PORT;
 
 app.listen(PORT, () => {
   console.log(`Listening on: http://localhost:${PORT}`);
