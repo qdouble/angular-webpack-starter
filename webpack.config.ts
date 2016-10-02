cb62f2bfafae5de9f12b0ebebfb06cfa02267222
Anthony <qdouble@gmail.com>
2016-10-02 16:27:54 -0700

    fix(config) cleanup ENV and add devserver flag

--- webpack.config.ts
+++ webpack.config.ts
@@ -28,6 +28,7 @@ const hasProcessFlag = require('./helpers.js').hasProcessFlag;
 const root = require('./helpers.js').root;
 
 const AOT = hasProcessFlag('AOT');
+const DEVSERVER = hasProcessFlag('DEVSERVER');
 const isProd = hasProcessFlag('PROD');
 
 let port: number;
@@ -41,7 +42,7 @@ const PORT = port;
 
 console.log('PRODUCTION BUILD: ', isProd);
 console.log('AOT: ', AOT);
-if (ENV === 'webdev') {
+if (DEVSERVER) {
   console.log(`Starting dev server on: http://${HOST}:${PORT}`);
 }
 
