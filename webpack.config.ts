c83bbac24ea6b6398c033be3bc4b208014b7085e
Anthony <qdouble@gmail.com>
2016-10-03 13:14:41 -0700

    refactor(assets) move to client_files folder
    This is to address this issue: https://github.com/qdouble/angular2webpack2-starter/pull/90

--- webpack.config.ts
+++ webpack.config.ts
@@ -98,15 +98,15 @@ const DLL_VENDORS = [
 ];
 
 const COPY_FOLDERS = [
-  { from: 'src/assets', to: 'assets' },
-  { from: 'node_modules/hammerjs/hammer.min.js', to: '' },
-  { from: 'node_modules/hammerjs/hammer.min.js.map', to: '' },
-  { from: 'dll', to: '' },
+  { from: 'src/assets'},
+  { from: 'node_modules/hammerjs/hammer.min.js'},
+  { from: 'node_modules/hammerjs/hammer.min.js.map'},
+  { from: 'dll'},
   ...MY_COPY_FOLDERS
 ];
 
 if (!DEV_SERVER) {
-  COPY_FOLDERS.unshift({ from: 'src/index.html', to: '' });
+  COPY_FOLDERS.unshift({ from: 'src/index.html'});
 }
 
 const commonConfig = function webpackConfig(): WebpackConfig {
