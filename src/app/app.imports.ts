b31dac2a0d8bc28752a4e7f5c7c99511ed185ac3
Anthony <qdouble@gmail.com>
2016-10-06 13:22:06 -0700

    feat: Implement preloading for lazy modules, use beta (#107)
    * update to 2.10-beta.0
    
    * implement preloading

--- src/app/app.imports.ts
+++ src/app/app.imports.ts
@@ -1,10 +1,10 @@
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 
 import { routes } from './app.routing';
 
 export const APP_IMPORTS = [
   ReactiveFormsModule,
-  RouterModule.forRoot(routes)
+  RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
 ];
 
