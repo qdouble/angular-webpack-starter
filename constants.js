307c63960361aaef61ba06d1bd6c9bb97e51d217
Anthony <qdouble@gmail.com>
2016-10-05 19:27:25 -0700

    add constant for devServer watchOptions (#102)

--- constants.js
+++ constants.js
@@ -25,6 +25,17 @@ exports.DEV_SERVER_PROXY_CONFIG = {
 exports.DEV_SOURCE_MAPS = 'eval';
 exports.PROD_SOURCE_MAPS = 'source-map';
 
+/**
+ * Set watch options for Dev Server. For better HMR performance, you can 
+ * try setting poll to 1000 or as low as 300 and set aggregateTimeout to as low as 0. 
+ * These settings will effect CPU usage, so optimal setting will depend on your dev environment.
+ * https://github.com/webpack/docs/wiki/webpack-dev-middleware#watchoptionsaggregatetimeout
+ */
+exports.DEV_SERVER_WATCH_OPTIONS = {
+  poll: undefined,
+  aggregateTimeout: 300
+}
+
 exports.EXCLUDE_SOURCE_MAPS = [
   // these packages have problems with their sourcemaps
   root('node_modules/@angular'),
