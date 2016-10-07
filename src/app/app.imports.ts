4259d4f4df49c8cd240d030bcfa24ce06235d9ac
Anthony <qdouble@gmail.com>
2016-10-06 23:30:57 -0700

    remove module preloading until bug fixed, update compiler-cli (#109)
    * remove preload modules until bug is fixed
    
    * update compiler-cli

--- src/app/app.imports.ts
+++ src/app/app.imports.ts
@@ -1,10 +1,10 @@
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule, PreloadAllModules } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 import { routes } from './app.routing';
 
 export const APP_IMPORTS = [
   ReactiveFormsModule,
-  RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  RouterModule.forRoot(routes)
 ];
 
