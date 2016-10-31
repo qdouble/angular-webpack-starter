b668a9d12947c23f509e9c4666dc1211add381b8
Anthony <qdouble@gmail.com>
2016-10-31 11:23:48 -0700

    fix @ngrx store logMonitor time-traveling

--- src/app/app.imports.ts
+++ src/app/app.imports.ts
@@ -5,11 +5,25 @@ import { MaterialModule } from '@angular/material';
 import { EffectsModule } from '@ngrx/effects';
 import { RouterStoreModule } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { useLogMonitor } from '@ngrx/store-log-monitor';
 
 import { routes } from './app.routing';
 import { rootReducer } from './reducers';
 import { StoreDevToolsModule } from './features/store-devtools.module';
 import { UserEffects } from './user/user.effects';
+
+const STORE_DEV_TOOLS_IMPORTS = [];
+if (ENV === 'development' && !AOT &&
+  ['monitor', 'both'].includes(STORE_DEV_TOOLS) // set in constants.js file in project root
+) STORE_DEV_TOOLS_IMPORTS.push(...[
+  StoreDevtoolsModule.instrumentStore({
+    monitor: useLogMonitor({
+      visible: true,
+      position: 'right'
+    })
+  })
+]);
 
 export const APP_IMPORTS = [
   EffectsModule.run(UserEffects),
@@ -17,7 +31,8 @@ export const APP_IMPORTS = [
   ReactiveFormsModule,
   RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   RouterStoreModule.connectRouter(),
-  StoreDevToolsModule,
-  StoreModule.provideStore(rootReducer)
+  StoreModule.provideStore(rootReducer),
+  STORE_DEV_TOOLS_IMPORTS,
+  StoreDevToolsModule
 ];
 
