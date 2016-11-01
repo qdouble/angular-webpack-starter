11a34246e893d4d8fea0172ada7ffa7480f0a7af
Anthony <qdouble@gmail.com>
2016-11-01 05:59:25 -0700

    feature: add webpack-bundle-analyzer (#152)
    runs only in production builds

--- webpack.config.ts
+++ webpack.config.ts
@@ -27,6 +27,7 @@ const { ForkCheckerPlugin } = require('awesome-typescript-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const NamedModulesPlugin = require('webpack/lib/NamedModulesPlugin');
 const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const { hasProcessFlag, root, testDll } = require('./helpers.js');
 
@@ -182,7 +183,8 @@ const clientConfig = function webpackConfig(): WebpackConfig {
         threshold: 10240,
         minRatio: 0.8
       }),
-      ...MY_CLIENT_PRODUCTION_PLUGINS
+      new BundleAnalyzerPlugin(),
+      ...MY_CLIENT_PRODUCTION_PLUGINS,
     );
   }
 
