41030a2a14bd050bcd39b4e8a9b866ac63316f13
Anthony <qdouble@gmail.com>
2016-11-03 10:52:52 -0700

    add main.scss/css for setting global styles

--- webpack.config.ts
+++ webpack.config.ts
@@ -91,6 +91,7 @@ const COPY_FOLDERS = [
   { from: 'src/assets', to: 'assets' },
   { from: 'node_modules/hammerjs/hammer.min.js' },
   { from: 'node_modules/hammerjs/hammer.min.js.map' },
+  { from: 'src/app/main.css' },
   { from: 'src/app/styles.css' },
   ...MY_COPY_FOLDERS
 ];
