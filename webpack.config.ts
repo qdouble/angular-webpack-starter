da684cde520606313967738ca598ef88466a0d44
Anthony <qdouble@gmail.com>
2016-11-03 10:52:52 -0700

    add main.scss/css for setting global styles

--- webpack.config.ts
+++ webpack.config.ts
@@ -90,6 +90,7 @@ const DLL_VENDORS = [
 
 const COPY_FOLDERS = [
   { from: 'src/assets', to: 'assets' },
+  { from: 'src/app/main.css' },
   { from: 'src/app/styles.css' },
   ...MY_COPY_FOLDERS
 ];
