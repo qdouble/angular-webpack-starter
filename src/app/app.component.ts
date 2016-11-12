6034f3bc4db563575e353eac62638b3686078d3f
Anthony <qdouble@gmail.com>
2016-11-12 11:33:00 -0800

    remove sideNav patch (fixed in material update)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { MdSidenav } from '@angular/material';
 
 import { views } from './app-nav-views';
 import { MOBILE } from './services/constants';
@@ -10,27 +9,18 @@ import { MOBILE } from './services/constants';
   styleUrls: ['./app.component.css'],
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   showMonitor = (ENV === 'development' && !AOT &&
     ['monitor', 'both'].includes(STORE_DEV_TOOLS) // set in constants.js file in project root
   );
   mobile = MOBILE;
   sideNavMode = MOBILE ? 'over' : 'side';
   views = views;
-  @ViewChild(MdSidenav) sidenav: MdSidenav;
 
   constructor(
     public route: ActivatedRoute,
     public router: Router
   ) { }
-
-  ngOnInit() {
-    // Prevent sidenav from generating promise error when closing
-    this.sidenav._onTransitionEnd = function () {
-      this._openPromise = null;
-      this._closePromise = null;
-    };
-  }
 
   activateEvent(event) {
     if (ENV === 'development') {
