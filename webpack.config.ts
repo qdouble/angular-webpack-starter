effe1211a3dfddecd5e0c2fe9ffeb65a1a7d564d
Anthony <qdouble@gmail.com>
2016-12-18 19:56:25 -0800

    fixed broken package.json, enabled es2015 modules for proper tree shaking (#214)

--- webpack.config.ts
+++ webpack.config.ts
@@ -124,7 +124,7 @@ const commonConfig = function webpackConfig(): WebpackConfig {
         test: /\.ts$/,
         loaders: [
           '@angularclass/hmr-loader',
-          'awesome-typescript-loader',
+          'awesome-typescript-loader?tsconfig=tsconfig.webpack.json',
           'angular2-template-loader',
           'angular-router-loader?loader=system&genDir=src/compiled/src/app&aot=' + AOT
         ],
