fd54fc6f285c36d291cb1cc13bfa03b508cd2dc4
Colin Skow <colinskow@gmail.com>
2016-12-21 10:18:30 -0800

    created proper e2e test (#219)

--- e2e/app.e2e.ts
+++ e2e/app.e2e.ts
@@ -1,6 +1,6 @@
-import { browser } from 'protractor';
+import { browser, element, by } from 'protractor';
 
-describe('App', () => {
+describe('Dashboard', () => {
 
   beforeEach(() => {
     browser.get('/');
@@ -11,5 +11,61 @@ describe('App', () => {
     let result  = 'Angular 2 App';
     expect(subject).toEqual(result);
   });
+
+  it('should have header', () => {
+    let subject = element(by.tagName('header')).element(by.tagName('h2')).getText();
+    let result  = 'Angular Starter App';
+    expect(subject).toEqual(result);
+  });
+
+  it('should welcome the user', () => {
+    let subject = element(by.tagName('my-dashboard'))
+      .element(by.tagName('h3')).getText();
+    let result  = 'Welcome to the Dashboard Angular User!';
+    expect(subject).toEqual(result);
+  });
+
+  it('should edit the name', () => {
+    let dashboard = element(by.tagName('my-dashboard'));
+    let input = dashboard.element(by.tagName('input'));
+    input.clear();
+    input.sendKeys('Boss');
+    let button = dashboard.element(by.buttonText('Update store'));
+    button.click();
+    let subject = element(by.tagName('my-dashboard'))
+      .element(by.tagName('h3')).getText();
+    let result  = 'Welcome to the Dashboard Boss!';
+    expect(subject).toEqual(result);
+  });
+
+});
+
+describe('Lazy', () => {
+
+  it('should lazy load a module', () => {
+    browser.get('/lazy');
+    let subject = element(by.tagName('my-lazy'));
+    expect(subject.isPresent()).toBe(true);
+  })
+
+});
+
+describe('Sync', () => {
+
+  it('should load a syncronous module', () => {
+    browser.get('/sync');
+    let subject = element(by.tagName('my-sync'));
+    expect(subject.isPresent()).toBe(true);
+  })
+
+});
+
+describe('Not Found', () => {
+
+  it('should display a 404 error', () => {
+    browser.get('/wronglink');
+    let subject = element(by.tagName('my-not-found'));
+    expect(subject.isPresent()).toBe(true);
+  })
 
 });
