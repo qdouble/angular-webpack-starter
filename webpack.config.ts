b0c3d4a326e383b1a7ec72e61a6bbc828db0c359
Fabien Rogeret <fabien.rogeret@gmail.com>
2017-01-08 20:13:18 -0500

    Update angular-router-loader to version 0.5.0 (#231)

--- webpack.config.ts
+++ webpack.config.ts
@@ -126,7 +126,7 @@ const commonConfig = function webpackConfig(): WebpackConfig {
           '@angularclass/hmr-loader',
           'awesome-typescript-loader?{configFileName: "tsconfig.webpack.json"}',
           'angular2-template-loader',
-          'angular-router-loader?loader=system&genDir=src/compiled/src/app&aot=' + AOT
+          'angular-router-loader?loader=system&genDir=compiled&aot=' + AOT
         ],
         exclude: [/\.(spec|e2e|d)\.ts$/]
       },
@@ -275,7 +275,7 @@ const clientConfig = function webpackConfig(): WebpackConfig {
   }
 
   config.devServer = {
-    contentBase: AOT ? './src/compiled' : './src',
+    contentBase: AOT ? './compiled' : './src',
     port: CONSTANTS.PORT,
     historyApiFallback: {
       disableDotRule: true,
