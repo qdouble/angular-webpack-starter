0b5c758b2aa552111f9f05006dcf27953eb1d9cb
Anthony <qdouble@gmail.com>
2017-04-30 21:09:36 -0700

    add web animation polyfill

--- src/polyfills.browser.aot.ts
+++ src/polyfills.browser.aot.ts
@@ -2,3 +2,4 @@ import 'core-js/es7/reflect';
 import 'core-js/client/shim';
 import 'zone.js/dist/zone';
 import 'ts-helpers';
+import 'web-animations-js/web-animations.min';
--- src/polyfills.browser.ts
+++ src/polyfills.browser.ts
@@ -4,3 +4,4 @@ import 'core-js/client/shim';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
 import 'ts-helpers';
+import 'web-animations-js/web-animations.min';
