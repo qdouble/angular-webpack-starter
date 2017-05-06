408d8592697825963cc1b3cb15db2ca705323100
Anthony <qdouble@gmail.com>
2017-05-05 23:38:55 -0700

    update packages

--- src/app/app.imports.ts
+++ src/app/app.imports.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { IdlePreload, IdlePreloadModule } from '@angularclass/idle-preload';
@@ -28,6 +29,7 @@ if (ENV === 'development' && !AOT &&
 ]);
 
 export const APP_IMPORTS = [
+  BrowserAnimationsModule,
   EffectsModule.run(UserEffects),
   NgbModule.forRoot(),
   ReactiveFormsModule,
