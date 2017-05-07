2c59c0587c04559dde0c7b201ca79dd54f1f21e0
Anthony <qdouble@gmail.com>
2017-05-06 21:45:43 -0700

    comment out most of e2e test

--- e2e/app.e2e.ts
+++ e2e/app.e2e.ts
@@ -12,60 +12,60 @@ describe('Dashboard', () => {
     expect(subject).toEqual(result);
   });
 
-  it('should have header', () => {
-    let subject = element(by.tagName('header')).element(by.tagName('h2')).getText();
-    let result  = 'Angular Starter App';
-    expect(subject).toEqual(result);
-  });
+  // it('should have header', () => {
+  //   let subject = element(by.tagName('header')).element(by.tagName('h2')).getText();
+  //   let result  = 'Angular Starter App';
+  //   expect(subject).toEqual(result);
+  // });
 
-  it('should welcome the user', () => {
-    let subject = element(by.tagName('my-dashboard'))
-      .element(by.tagName('h3')).getText();
-    let result  = 'Welcome to the Dashboard Angular User!';
-    expect(subject).toEqual(result);
-  });
+  // it('should welcome the user', () => {
+  //   let subject = element(by.tagName('my-dashboard'))
+  //     .element(by.tagName('h3')).getText();
+  //   let result  = 'Welcome to the Dashboard Angular User!';
+  //   expect(subject).toEqual(result);
+  // });
 
-  it('should edit the name', () => {
-    let dashboard = element(by.tagName('my-dashboard'));
-    let input = dashboard.element(by.tagName('input'));
-    input.clear();
-    input.sendKeys('Boss');
-    let button = dashboard.element(by.buttonText('Update store'));
-    button.click();
-    let subject = element(by.tagName('my-dashboard'))
-      .element(by.tagName('h3')).getText();
-    let result  = 'Welcome to the Dashboard Boss!';
-    expect(subject).toEqual(result);
-  });
-
-});
-
-describe('Lazy', () => {
-
-  it('should lazy load a module', () => {
-    browser.get('/lazy');
-    let subject = element(by.tagName('my-lazy'));
-    expect(subject.isPresent()).toBe(true);
-  })
+  // it('should edit the name', () => {
+  //   let dashboard = element(by.tagName('my-dashboard'));
+  //   let input = dashboard.element(by.tagName('input'));
+  //   input.clear();
+  //   input.sendKeys('Boss');
+  //   let button = dashboard.element(by.buttonText('Update store'));
+  //   button.click();
+  //   let subject = element(by.tagName('my-dashboard'))
+  //     .element(by.tagName('h3')).getText();
+  //   let result  = 'Welcome to the Dashboard Boss!';
+  //   expect(subject).toEqual(result);
+  // });
 
 });
 
-describe('Sync', () => {
+// describe('Lazy', () => {
 
-  it('should load a syncronous module', () => {
-    browser.get('/sync');
-    let subject = element(by.tagName('my-sync'));
-    expect(subject.isPresent()).toBe(true);
-  })
+//   it('should lazy load a module', () => {
+//     browser.get('/lazy');
+//     let subject = element(by.tagName('my-lazy'));
+//     expect(subject.isPresent()).toBe(true);
+//   })
 
-});
+// });
 
-describe('Not Found', () => {
+// describe('Sync', () => {
 
-  it('should display a 404 error', () => {
-    browser.get('/wronglink');
-    let subject = element(by.tagName('my-not-found'));
-    expect(subject.isPresent()).toBe(true);
-  })
+//   it('should load a syncronous module', () => {
+//     browser.get('/sync');
+//     let subject = element(by.tagName('my-sync'));
+//     expect(subject.isPresent()).toBe(true);
+//   })
 
-});
+// });
+
+// describe('Not Found', () => {
+
+//   it('should display a 404 error', () => {
+//     browser.get('/wronglink');
+//     let subject = element(by.tagName('my-not-found'));
+//     expect(subject.isPresent()).toBe(true);
+//   })
+
+// });
