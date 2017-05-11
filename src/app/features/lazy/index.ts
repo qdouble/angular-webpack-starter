6d52d6a4705dc0eca446324d0990e2b6ec093663
Anthony <qdouble@gmail.com>
2017-05-10 22:44:30 -0700

    make last pull request match project style

--- src/app/features/lazy/index.ts
+++ src/app/features/lazy/index.ts
@@ -1,21 +1,24 @@
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+import { LazyActions } from './lazy.actions';
 import { routes } from './lazy.routing';
 
 import { LazyComponent } from './lazy.component';
 
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes),
-  ],
-  declarations: [
-    LazyComponent
-  ]
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+    ],
+    declarations: [
+        LazyComponent
+    ],
+    providers: [
+        LazyActions
+    ]
 })
 
-export class LazyModule {}
-
+export class LazyModule { }
