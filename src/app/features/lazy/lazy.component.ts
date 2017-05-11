cf6db94d87efafb04f6594c689f06fff1248e698
Sumanth Shankar <careers.s.shankar@gmail.com>
2017-05-11 07:33:28 +0200

    Adds reducer to lazy loaded routes and persists state when hmr is enabled.  (#277)
    * changes to check lazy route lazy reducer and to check state persistence
    
    *  remove extra exclamatory mark
    
    *  working copy of lazy loading
    
    * remove code duplication
    
    * fix tests and universal
    
    * load BrowserAnimationsModule in app.module in dev
    
    *  revert @ngrx/store to 2.2.2
    
    * remove unused code, change functions to es6
    
    * changes to check lazy route lazy reducer and to check state persistence
    
     remove extra exclamatory mark
    
    *  working copy of lazy loading
    
    remove code duplication
    
     revert @ngrx/store to 2.2.2
    
    remove unused code, change functions to es6
    
    *  fix lint

--- src/app/features/lazy/lazy.component.ts
+++ src/app/features/lazy/lazy.component.ts
@@ -1,8 +1,36 @@
+import {
+  StoreWithLazy,
+  lazyStoreFactory,
+  INCREMENT,
+  DECREMENT,
+  RESET
+} from './lazy.reducer';
+import { Observable } from 'rxjs/Observable';
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'my-lazy',
-  templateUrl: './lazy.component.html'
+  templateUrl: './lazy.component.html',
+  providers: [ { provide: StoreWithLazy, useFactory: lazyStoreFactory, deps: [Store]}]
 })
 
-export class LazyComponent {}
+export class LazyComponent {
+  counter: Observable<number>;
+
+  constructor(private store: StoreWithLazy) {
+    this.counter = store.select(s => s.lazy.counter);
+  }
+
+  increment() {
+    this.store.dispatch({ type: INCREMENT });
+  }
+
+  decrement() {
+    this.store.dispatch({ type: DECREMENT });
+  }
+
+  reset() {
+    this.store.dispatch({ type: RESET });
+  }
+}
