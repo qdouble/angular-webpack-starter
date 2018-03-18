4fa0b36648d02e7b9c222816683073578a03001b
Anthony <qdouble@gmail.com>
2018-03-17 21:35:30 -0500

    disable aot sourcemaps

--- webpack.aot.js
+++ webpack.aot.js
@@ -13,7 +13,7 @@ function getAotPlugin(platform, aot) {
   return new AngularCompilerPlugin({
     tsConfigPath: aot ? aotTsconfigs[platform] : tsconfigs[platform],
     skipCodeGeneration: !aot,
-    sourceMap: true
+    sourceMap: false
   });
 }
 
